4e3b2bde358d0ffb115eb2349d7b063d82ecf097
Sani Elfishawy <sani@sbcglobal.net>
2014-04-01 06:31:02 -0700

    Video status

--- app/platforms/android/src/com/noplanbees/tbm/FriendFactory.java
+++ app/platforms/android/src/com/noplanbees/tbm/FriendFactory.java
@@ -7,20 +7,20 @@ import android.view.View;
 public class FriendFactory extends ActiveModelFactory{
 
 	public static FriendFactory instance = null;
-	
+
 	public static FriendFactory getFactoryInstance(){
 		if ( instance == null ) 
 			instance = new FriendFactory();
 		return instance;
 	}
-	
+
 	@Override
 	protected Friend makeInstance() {
 		Friend i = new Friend();
 		i.init();
 		instances.add(i);
 		return i;	}
-	
+
 	public static Friend getFriendFromFrame(View v){
 		Integer viewId = v.getId();
 		return (Friend) FriendFactory.getFactoryInstance().findWhere("frameId", viewId.toString());
@@ -30,11 +30,15 @@ public class FriendFactory extends ActiveModelFactory{
 		Friend f = null;
 		Bundle extras = intent.getExtras();
 		if (extras != null){
-			if (extras.get("friendId") != null){
+			if ( extras.get("friendId") != null ){
 				f = (Friend) find(extras.getString("friendId"));
-			} else if (extras.get("receiverId") != null){
+			} else if ( extras.get("receiverId") != null ){
 				f = (Friend) find(extras.getString("receiverId"));
-			}
+			} else if ( extras.get("from_id") != null ){
+				f = (Friend) find(extras.getString("from_id"));
+			} else if ( extras.get("to_id") != null ){
+				f = (Friend) find(extras.getString("to_id"));
+			}		
 		}
 		return f;
 	}
--- app/platforms/android/src/com/noplanbees/tbm/GcmIntentService.java
+++ app/platforms/android/src/com/noplanbees/tbm/GcmIntentService.java
@@ -14,49 +14,89 @@ import com.google.android.gms.gcm.GoogleCloudMessaging;
  */
 public class GcmIntentService extends IntentService {
 	private final String TAG = "GCM " + this.getClass().getSimpleName();
-    NotificationCompat.Builder builder;
+	NotificationCompat.Builder builder;
 
-    public GcmIntentService() {
-        super("GcmIntentService");
-        // Pull our models from storage so we can use them in handling the notification
-        ActiveModelsHandler.ensureAll();
-    }
+	public GcmIntentService() {
+		super("GcmIntentService");
+		// Pull our models from storage so we can use them in handling the notification
+		ActiveModelsHandler.ensureAll();
+	}
 
-    @Override
-    protected void onHandleIntent(Intent intent) {
-        Bundle extras = intent.getExtras();
-        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
-        // The getMessageType() intent parameter must be the intent you received
-        // in your BroadcastReceiver.
-        String messageType = gcm.getMessageType(intent);
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		Bundle extras = intent.getExtras();
+		GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+		// The getMessageType() intent parameter must be the intent you received
+		// in your BroadcastReceiver.
+		String messageType = gcm.getMessageType(intent);
 
-        if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
-            /*
-             * Filter messages based on message type. Since it is likely that GCM
-             * will be extended in the future with new message types, just ignore
-             * any message types you're not interested in, or that you don't
-             * recognize.
-             */
-            if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
-            	//Not used
-            } else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
-            	//Not used
-            } else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
-                Log.i(TAG, "onHandleIntent: extras = " + extras.toString());
-                String friendId = extras.getString("from_id");
-                FriendFactory ff = FriendFactory.getFactoryInstance();
-                Friend friend = (Friend) ff.find(friendId);
-                friend.downloadVideo();
-                ActiveModelsHandler.saveFriend();  
-                sendHomeActivityIntent(friend);
-            }
-        }
-        // Release the wake lock provided by the WakefulBroadcastReceiver.
-        GcmBroadcastReceiver.completeWakefulIntent(intent);
-    }
+		if (!extras.isEmpty()) {  // has effect of unparcelling Bundle
+			/*
+			 * Filter messages based on message type. Since it is likely that GCM
+			 * will be extended in the future with new message types, just ignore
+			 * any message types you're not interested in, or that you don't
+			 * recognize.
+			 */
+			if (GoogleCloudMessaging.MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+				//Not used
+			} else if (GoogleCloudMessaging.MESSAGE_TYPE_DELETED.equals(messageType)) {
+				//Not used
+			} else if (GoogleCloudMessaging.MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+				Log.i(TAG, "onHandleIntent: extras = " + extras.toString());
 
-    private void sendHomeActivityIntent(Friend friend){
-        Log.i(TAG, "sendNotification: Sending intent to start home activity");
+				if ( extras.getString("type").equalsIgnoreCase("video_recieved") ){
+					handleVideoReceived(intent);	
+				} else if ( extras.getString("type").equalsIgnoreCase("video_status_update") ){
+					handleVideoStatusUpdate(intent);
+				}
+
+
+			}
+		}
+		// Release the wake lock provided by the WakefulBroadcastReceiver.
+		GcmBroadcastReceiver.completeWakefulIntent(intent);
+	}
+
+	private void handleVideoStatusUpdate(Intent intent) {
+		String status = intent.getExtras().getString("status");
+		
+		FriendFactory ff = ActiveModelsHandler.ensureFriend();
+		if (ff == null){
+			Log.e(TAG, "handleVideoStatusUpdate: no friend model.");
+			return;
+		}
+		
+		Friend friend = ff.getFriendFromIntent(intent);
+		if (friend == null){
+			Log.e(TAG, "handleVideoStatusUpdate: friend found." );
+			return;
+		}
+		
+		Intent i = new Intent(this, HomeActivity.class);
+		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		Bundle extras = new Bundle();
+		extras.putInt(IntentHandler.INTENT_TYPE_KEY, IntentHandler.TYPE_VIDEO_STATUS_UPDATE);
+		extras.putString("friendId", friend.getId());
+		
+		if ( status.equalsIgnoreCase("downloaded")){
+			extras.putInt(VideoStatusHandler.STATUS_KEY, VideoStatusHandler.DOWNLOADED);
+		} else if ( status.equalsIgnoreCase("viewed") ){
+			extras.putInt(VideoStatusHandler.STATUS_KEY, VideoStatusHandler.VIEWED);
+		}
+
+		i.putExtras(extras);
+		startActivity(i);
+	}
+
+	private void handleVideoReceived(Intent intent) {
+		Friend friend = FriendFactory.getFactoryInstance().getFriendFromIntent(intent);
+		friend.downloadVideo();
+		ActiveModelsHandler.saveFriend();  
+		sendHomeActivityIntent(friend);
+	}
+
+	private void sendHomeActivityIntent(Friend friend){
+		Log.i(TAG, "sendNotification: Sending intent to start home activity");
 		Intent i = new Intent(this, HomeActivity.class);
 		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 		Bundle extras = new Bundle();
@@ -64,7 +104,7 @@ public class GcmIntentService extends IntentService {
 		extras.putString("friendId", friend.getId());
 		i.putExtras(extras);
 		startActivity(i);
-    }
+	}
 
 }
 
