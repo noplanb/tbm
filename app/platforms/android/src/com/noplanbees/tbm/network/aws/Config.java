8144099e12048b06655b0dff65b271c4d309f813
ABoyko <aboyko@codeminders.com>
2015-01-02 13:52:17 +0200

    updated credentials for ASW S3

--- app/platforms/android/src/com/noplanbees/tbm/network/aws/Config.java
+++ app/platforms/android/src/com/noplanbees/tbm/network/aws/Config.java
@@ -1,8 +1,8 @@
 package com.noplanbees.tbm.network.aws;
 
 public class Config {
-	public static final String SECRET_KEY = "saLwmv761SKqGcXxKx9Y637dGJrjOvNz3KugTqLP";
-	public static final String ACCESS_KEY_ID = "AKIAJV553QLVAKL75IAA";
+	public static final String SECRET_KEY = "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";
+	public static final String ACCESS_KEY_ID = "AKIAI2XZARL4UJKO3NWQ";
 	
 	public static final String BUCKET_NAME = "com.zazo.videos";
 }
