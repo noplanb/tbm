398d25aa246dc6736dc5cd2e1b8476b04a8bca64
Serhii Kamenkovych <skamenkovych@codeminders.com>
2015-03-11 11:14:24 +0200

    Improved stability of thumbnail creation. If it is broken video status will be marked as FAILED_PERMANENTLY

---
+++ app/platforms/android/src/com/zazoapp/client/multimedia/ThumbnailRetriever.java
@@ -0,0 +1,96 @@
+package com.zazoapp.client.multimedia;
+
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.util.Log;
+
+/**
+ * Uses native Android metadata retriever to get thumbnail or notify error
+ * Created by skamenkovych@codeminders.com on 3/10/2015.
+ */
+public class ThumbnailRetriever {
+    private static final String TAG = ThumbnailRetriever.class.getSimpleName();
+
+    private MediaMetadataRetriever nativeRetriever = new MediaMetadataRetriever();
+
+    private String errorMessage;
+    boolean nativeWorks = true;
+
+    /**
+     * Gets thumbnail for video located in path.
+     * If operation is wrong it throws {@code ThumbnailBrokenException}
+     * @param path video file location
+     * @return thumbnail bitmap
+     */
+    public Bitmap getThumbnail(String path) throws ThumbnailBrokenException {
+        //ThumbnailUtils.createVideoThumbnail()
+        Bitmap thumb = null;
+        try {
+            try {
+                nativeRetriever.setDataSource(path);
+            } catch (IllegalArgumentException e) {
+                markFailed("native: Error setting datasource", e.toString());
+                return null;
+            }
+
+            String time = nativeRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+            if (time == null) {
+                markFailed("native: Error getting duration");
+                return null;
+            }
+            long nativeDuration = Long.parseLong(time);
+            long pos = getPos(nativeDuration);
+            thumb = nativeRetriever.getFrameAtTime(pos*1000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
+            if (thumb == null) {
+                thumb = nativeRetriever.getFrameAtTime(nativeDuration*1000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
+                if (thumb == null) {
+                    Log.e(TAG, "native: Error getting end frame");
+                    thumb = nativeRetriever.getFrameAtTime();
+                    if (thumb == null) {
+                        markFailed("native: Error getting representative frame");
+                    }
+                }
+            }
+        } finally {
+            verifyAndRelease();
+            return thumb;
+        }
+    }
+
+    /**
+     * ensure
+     */
+    private void verifyAndRelease() throws ThumbnailBrokenException {
+        nativeRetriever.release();
+        if (!nativeWorks) {
+            StringBuilder message = new StringBuilder();
+            message.append("Thumbnail has not been retrieved\n").append(errorMessage);
+            throw new ThumbnailBrokenException(message.toString());
+        } else {
+            Log.i(TAG, "Thumbnail retrieved successfully");
+        }
+    }
+
+    private void markFailed(String... messages) {
+        nativeWorks = false;
+        StringBuilder builder = new StringBuilder();
+        for (String message : messages) {
+            builder.append(message).append("\n");
+        }
+        errorMessage = builder.toString();
+    }
+
+    private static long getPos(long duration) {
+        return (duration > 2500) ? duration - 2000 : duration / 2;
+    }
+
+    public class ThumbnailBrokenException extends Exception {
+        public ThumbnailBrokenException() {
+            super();
+        }
+
+        public ThumbnailBrokenException(String detailMessage) {
+            super(detailMessage);
+        }
+    }
+}
