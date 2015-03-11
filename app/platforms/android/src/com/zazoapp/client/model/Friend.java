4a07bcc5dd1940389eb93f14d09a1eb4b79a34e7
Serhii Kamenkovych <skamenkovych@codeminders.com>
2015-03-11 11:14:24 +0200

    Improved stability of thumbnail creation. If it is broken video status will be marked as FAILED_PERMANENTLY

--- app/platforms/android/src/com/zazoapp/client/model/Friend.java
+++ app/platforms/android/src/com/zazoapp/client/model/Friend.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.media.MediaMetadataRetriever;
 import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,6 +14,7 @@ import com.zazoapp.client.Config;
 import com.zazoapp.client.RemoteStorageHandler;
 import com.zazoapp.client.debug.DebugConfig;
 import com.zazoapp.client.dispatch.Dispatch;
+import com.zazoapp.client.multimedia.ThumbnailRetriever;
 import com.zazoapp.client.network.FileDeleteService;
 import com.zazoapp.client.network.FileDownloadService;
 import com.zazoapp.client.network.FileTransferService;
@@ -429,28 +429,13 @@ public class Friend extends ActiveModel{
         }else{
             try {
                 String vidPath = videoFromPath(videoId);
-
-                MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-                retriever.setDataSource(vidPath);
-
-                String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
-                long duration = Long.parseLong( time );
-                Log.d(TAG, "Duration: " + duration);
-
-                long pos;
-                if(duration>2500)
-                    pos = duration - 2000;
-                else
-                    pos = duration/2;
-
-                Log.d(TAG, "Pos: " + pos);
-
-                Bitmap thumb = retriever.getFrameAtTime(pos*1000);
+                ThumbnailRetriever retriever = new ThumbnailRetriever();
+                Bitmap thumb = retriever.getThumbnail(vidPath);
                 File thumbFile = thumbFile(videoId);
                 FileOutputStream fos = FileUtils.openOutputStream(thumbFile);
                 thumb.compress(Bitmap.CompressFormat.PNG, 100, fos);
                 res = true;
-            } catch (IOException| RuntimeException e) {
+            } catch (IOException | RuntimeException | ThumbnailRetriever.ThumbnailBrokenException e) {
                 Dispatch.dispatch("createThumb: " + e.getMessage() + e.toString());
                 res = false;
             }
