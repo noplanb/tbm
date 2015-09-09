5750a74a9551c6cc76b38cd7316ed82c01bbc3ff
Serhii Kamenkovych <skamenkovych@codeminders.com>
2015-09-09 14:19:41 +0300

    Added class log to FileTransferServce logs

--- app/ZazoS3NetworkTest/src/com/zazoapp/s3networktest/network/FileTransferService.java
+++ app/ZazoS3NetworkTest/src/com/zazoapp/s3networktest/network/FileTransferService.java
@@ -16,6 +16,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public abstract class FileTransferService extends IntentService {
     private static final String TAG = FileTransferService.class.getSimpleName();
+    private final String OTAG = getClass().getSimpleName() + ": ";
     public static final String RESET_ACTION = "reset";
     private final static Integer MAX_RETRIES = 100;
 
@@ -67,7 +68,7 @@ public abstract class FileTransferService extends IntentService {
     @Override
     public void onStart(Intent intent, int startId) {
         if (RESET_ACTION.equals(intent.getAction())) {
-            Log.i(TAG, "Reset retries");
+            Log.i(TAG, OTAG + "Reset retries");
             retryCount.set(0);
             if (lock.tryLock()) {
                 try {
@@ -84,7 +85,7 @@ public abstract class FileTransferService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         lock.lock();
         try {
-            Log.i(TAG, "onHandleIntent");
+            Log.i(TAG, OTAG + "onHandleIntent");
             if (RESET_ACTION.equals(intent.getAction())) {
                 return;
             }
@@ -112,7 +113,7 @@ public abstract class FileTransferService extends IntentService {
     }
 
     public void reportStatus(Intent intent, int status) {
-        Log.i(TAG, "reportStatus");
+        Log.i(TAG, OTAG + "reportStatus: " + status);
         intent.setClass(getApplicationContext(), IntentHandlerService.class);
         intent.putExtra(IntentFields.STATUS_KEY, status);
         getApplicationContext().startService(intent);
@@ -120,11 +121,11 @@ public abstract class FileTransferService extends IntentService {
 
     private void retrySleep(Intent intent) throws InterruptedException {
         intent.putExtra(IntentFields.RETRY_COUNT_KEY, retryCount.incrementAndGet());
-        Log.i(TAG, "retry: " + retryCount.get());
+        Log.i(TAG, OTAG + "retry: " + retryCount.get());
 
         long sleepTime = sleepTime(retryCount.get());
 
-        Log.i(TAG, "Sleeping for: " + sleepTime + "ms");
+        Log.i(TAG, OTAG + "Sleeping for: " + sleepTime + "ms");
         if (reset.await(sleepTime, TimeUnit.MILLISECONDS)) {
             intent.putExtra(IntentFields.RETRY_COUNT_KEY, retryCount.get());
         }
