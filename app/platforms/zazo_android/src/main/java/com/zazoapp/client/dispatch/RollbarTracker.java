f07cb4d16c8a1ea69dee846846616971fdbdf689
Serhii Kamenkovych <skamenkovych@codeminders.com>
2016-06-08 14:12:41 +0300

    Improved Rollbar messages user info

--- app/platforms/zazo_android/src/main/java/com/zazoapp/client/dispatch/RollbarTracker.java
+++ app/platforms/zazo_android/src/main/java/com/zazoapp/client/dispatch/RollbarTracker.java
@@ -1,6 +1,8 @@
 package com.zazoapp.client.dispatch;
 
 import android.content.Context;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import com.google.gson.internal.LinkedTreeMap;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
@@ -94,24 +96,33 @@ public class RollbarTracker implements ErrorTracker {
     }
 
     private void setUpUser() {
+        JSONObject person = new JSONObject();
+        try {
+            person.put("id", "-");
+            person.put("username", "-");
+            person.put("phone_number", "-");
+            person.put("host", Config.getServerHost());
+            person.put("android_id", Settings.Secure.getString(TbmApplication.getInstance().getContentResolver(), Settings.Secure.ANDROID_ID));
+            TelephonyManager tm = (TelephonyManager) TbmApplication.getInstance().getSystemService(Context.TELEPHONY_SERVICE);
+            if (tm != null) {
+                person.put("device_id", tm.getDeviceId());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         User user = UserFactory.current_user();
         if (user != null && !user.getId().isEmpty()) {
             Phonenumber.PhoneNumber phone = user.getPhoneNumberObj();
             String phoneNumber = PhoneNumberUtil.getInstance().format(phone, PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL);
-            Rollbar.setPersonData(user.getId(), user.getFullName(), phoneNumber);
-            JSONObject person = new JSONObject();
             try {
                 person.put("id", user.getId());
                 person.put("username", user.getFullName());
                 person.put("phone_number", phoneNumber);
-                person.put("host", Config.getServerHost());
                 person.put("user_data", UserInfoCollector.collect(TbmApplication.getInstance()));
-                Rollbar.setPersonData(person);
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-        } else {
-            Rollbar.setPersonData("", "", "");
         }
+        Rollbar.setPersonData(person);
     }
 }
