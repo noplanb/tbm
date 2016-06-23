4da445b1fe7a06d1dd0c74fa663ffa5e8019bdc3
Serhii Kamenkovych <docanod@gmail.com>
2016-06-23 13:23:01 +0300

    1329 Clear FULLSCREEN flag when done

--- app/platforms/zazo_android/src/main/java/com/zazoapp/client/multimedia/PlayOptions.java
+++ app/platforms/zazo_android/src/main/java/com/zazoapp/client/multimedia/PlayOptions.java
@@ -6,7 +6,12 @@ package com.zazoapp.client.multimedia;
 public class PlayOptions {
     public static final int FULLSCREEN = 0x0001;
 
-    public static boolean isFullscreen(int options) {
+    public static boolean isFullscreen(@Player.PlayFlags int options) {
         return (options & FULLSCREEN) > 0;
+    }
+
+    public static @Player.PlayFlags int clearFlags(@Player.PlayFlags int options, @Player.PlayFlags int flags) {
+        options = options & ~flags;
+        return options;
     }
 }
--- app/platforms/zazo_android/src/main/java/com/zazoapp/client/multimedia/VideoPlayer.java
+++ app/platforms/zazo_android/src/main/java/com/zazoapp/client/multimedia/VideoPlayer.java
@@ -73,7 +73,7 @@ public class VideoPlayer implements OnCompletionListener, OnPreparedListener, Pl
     private ZoomController zoomController;
     private float mediaVolume = 1.0f;
     private WeakReference<View> targetViewRef;
-    @PlayFlags private int playFlags;
+    @PlayFlags private int playOptions;
 
     private Set<StatusCallbacks> statusCallbacks = new HashSet<StatusCallbacks>();
     private List<IncomingVideo> playingVideos;
@@ -150,7 +150,7 @@ public class VideoPlayer implements OnCompletionListener, OnPreparedListener, Pl
         friend = FriendFactory.getFactoryInstance().find(friendId);
 
         if (needToPlay) {
-            playFlags = options;
+            playOptions = options;
             zoomController.clearState();
             targetViewRef = new WeakReference<View>(view);
             setPlayerOverTargetView();
@@ -421,8 +421,11 @@ public class VideoPlayer implements OnCompletionListener, OnPreparedListener, Pl
                                         isSeekAllowed = true;
                                         videoRootLayout.animate().alpha(1).start();
                                         zoomController.setEnabled(true);
-                                        if (PlayOptions.isFullscreen(playFlags)) {
-                                            zoomController.animateToFullscreen();
+                                        if (PlayOptions.isFullscreen(playOptions)) {
+                                            if (!zoomController.zoomed) {
+                                                zoomController.animateToFullscreen();
+                                            }
+                                            playOptions = PlayOptions.clearFlags(playOptions, PlayOptions.FULLSCREEN);
                                         }
                                         VideoProgressBar.Scheme.SchemeBuilder schemeBuilder = new VideoProgressBar.Scheme.SchemeBuilder();
                                         for (int i = 0; i < numberOfVideos; i++) {
