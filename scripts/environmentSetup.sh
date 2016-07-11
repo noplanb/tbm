d45ca9c4bd8ba3ba692e4c4510ffa9ecca140a0d
Serhii Kamenkovych <skamenkovych@codeminders.com>
2016-07-11 14:09:43 +0300

    Fixed script for correct ABI

--- scripts/environmentSetup.sh
+++ scripts/environmentSetup.sh
@@ -12,7 +12,7 @@ function getAndroidSDK(){
     echo y | android update sdk -u -a -t build-tools-23.0.3 &&
     #echo y | android update sdk -u -a -t sys-img-x86-android-19 &&
     #echo y | android update sdk -u -a -t addon-google_apis-google-18 &&
-    echo no | android create avd -n testAVD -f -t android-23 --abi default/x86 &&
+    echo no | android create avd -n testAVD -f -t android-19 --abi default/armeabi-v7a &&
     touch $DEPS
   fi
 }
