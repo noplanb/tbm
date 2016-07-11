8f4f8476ae0784bdc06554305da67df373e56e71
Serhii Kamenkovych <skamenkovych@codeminders.com>
2016-07-11 14:05:28 +0300

    Fixed circle.yml

--- scripts/environmentSetup.sh
+++ scripts/environmentSetup.sh
@@ -7,7 +7,6 @@ function getAndroidSDK(){
   DEPS="$ANDROID_HOME/installed-dependencies"
 
   if [ ! -e $DEPS ]; then
-    cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
     echo y | android update sdk -u -a -t android-23 &&
     echo y | android update sdk -u -a -t platform-tools &&
     echo y | android update sdk -u -a -t build-tools-23.0.3 &&
